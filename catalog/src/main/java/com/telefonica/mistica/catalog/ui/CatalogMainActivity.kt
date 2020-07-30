ee152b7c63dcc25b1449062f7976f3679a5ab502
dpastor <dpastor@tuenti.com>
2020-07-30 15:46:30 +0200

    Use rows instead buttons

--- catalog/src/main/java/com/telefonica/mistica/catalog/ui/CatalogMainActivity.kt
+++ catalog/src/main/java/com/telefonica/mistica/catalog/ui/CatalogMainActivity.kt
@@ -1,43 +1,71 @@
 package com.telefonica.mistica.catalog.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.annotation.IdRes
-import com.telefonica.mistica.button.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.telefonica.mistica.catalog.R
+import com.telefonica.mistica.list.ListRowView
+import com.telefonica.mistica.list.layout.configureWithFullWidthLayout
+
 
 class CatalogMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_catalog)
-        configureButtons()
+
+        with(findViewById<RecyclerView>(R.id.list_sections)) {
+            configureWithFullWidthLayout()
+            adapter = SectionAdapter()
+        }
     }
 
-    private fun configureButtons() {
-        configureButton(R.id.button_buttons, Section.BUTTONS)
-        configureButton(R.id.button_texts, Section.TEXTS)
-        configureButton(R.id.button_inputs, Section.INPUTS)
-        configureButton(R.id.button_snackbars, Section.SNACKBARS)
-        configureButton(R.id.button_feedbacks, Section.FEEDBACKS)
-        configureButton(R.id.button_load_error_feedback, Section.LOAD_ERROR_FEEDBACK)
-        configureButton(R.id.button_pop_overs, Section.POPOVERS)
-        configureButton(R.id.button_badges, Section.BADGES)
-        configureButton(R.id.button_scroll_content_indicator, Section.SCROLL_CONTENT_INDICATOR)
-        configureButton(R.id.button_tags, Section.TAG)
-        configureButton(R.id.button_lists, Section.LISTS)
-        configureButton(R.id.button_headers, Section.HEADERS)
-        configureButton(R.id.button_others, Section.OTHERS)
-    }
+    class SectionAdapter : RecyclerView.Adapter<SectionAdapter.ViewHolder>() {
 
-    private fun configureButton(@IdRes buttonId: Int, sectionToOpen: Section) {
-        findViewById<Button>(buttonId).setOnClickListener {
-            Intent(this, ComponentCatalogActivity::class.java)
-                .putExtra(ComponentCatalogActivity.EXTRA_SECTION, sectionToOpen)
-                .let {
-                    startActivity(it)
+        private val sections: List<Pair<String, Section>> = listOf(
+            "Buttons" to Section.BUTTONS,
+            "Texts" to Section.TEXTS,
+            "Inputs" to Section.INPUTS,
+            "Snackbars" to Section.SNACKBARS,
+            "Feedbacks" to Section.FEEDBACKS,
+            "Load Error Feedback" to Section.LOAD_ERROR_FEEDBACK,
+            "PopOvers" to Section.POPOVERS,
+            "Badges" to Section.BADGES,
+            "Scroll Content indicator" to Section.SCROLL_CONTENT_INDICATOR,
+            "Tag" to Section.TAG,
+            "Lists" to Section.LISTS,
+            "Headers" to Section.HEADERS,
+            "Others" to Section.OTHERS
+        )
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val item: ListRowView = view as ListRowView
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_main_catalog, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val section = sections[position]
+            with(holder.item) {
+                setTitle(section.first)
+                setOnClickListener {
+                    Intent(context, ComponentCatalogActivity::class.java)
+                        .putExtra(ComponentCatalogActivity.EXTRA_SECTION, section.second)
+                        .let {
+                            context.startActivity(it)
+                        }
                 }
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return sections.size
         }
     }
 }
