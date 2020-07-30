20b6afef2ba47e50c75ae00131d86dbfc7899af1
dpastor <dpastor@tuenti.com>
2020-07-30 14:22:43 +0200

    Add Example app

---
+++ catalog/src/main/java/com/telefonica/mistica/catalog/ui/CatalogMainActivity.kt
@@ -0,0 +1,13 @@
+package com.telefonica.mistica.catalog.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.telefonica.mistica.catalog.R
+
+class CatalogMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_catalog)
+    }
+}
