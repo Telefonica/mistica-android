0a598fb079606c92fbea1d46dab06f8947a7cc5e
Jesús Latorre <jesus.latorreasensio@tuenti.com>
2020-08-26 15:59:01 +0200

    SUBO-5037 Add option to show loading in feedback first button

--- library/src/main/java/com/telefonica/mistica/feedback/screen/view/FeedbackScreenView.kt
+++ library/src/main/java/com/telefonica/mistica/feedback/screen/view/FeedbackScreenView.kt
@@ -21,6 +21,7 @@ import androidx.annotation.LayoutRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.airbnb.lottie.LottieAnimationView
 import com.telefonica.mistica.R
+import com.telefonica.mistica.button.ProgressButton
 import com.telefonica.mistica.feedback.screen.haptics.HapticFeedbackType
 import com.telefonica.mistica.feedback.screen.haptics.performHapticFeedback
 import com.telefonica.mistica.util.background.GradientBackgroundFactory
@@ -42,12 +43,13 @@ class FeedbackScreenView : ConstraintLayout {
     private lateinit var subtitle: TextView
     private lateinit var customContentContainer: FrameLayout
     private lateinit var buttonsContainer: LinearLayout
-    private lateinit var firstButton: Button
+    private lateinit var firstButton: ProgressButton
     private lateinit var secondButton: Button
 
     private var type: Int = TYPE_INFO
     private var titleText: CharSequence = ""
     private var subtitleText: CharSequence = ""
+    private var isLoading: Boolean = false
 
     @LayoutRes
     private var customContentLayout: Int? = null
@@ -100,7 +102,7 @@ class FeedbackScreenView : ConstraintLayout {
 
     fun setFeedbackFirstButtonText(text: CharSequence) {
         firstButtonText = text
-        firstButton.text = text
+        firstButton.setText(text)
         firstButton.visibility = if (text.isBlank()) View.GONE else View.VISIBLE
     }
 
@@ -130,6 +132,11 @@ class FeedbackScreenView : ConstraintLayout {
     }
 
     fun getFirstButtonText(): String = firstButtonText.toString()
+
+    fun setIsLoading(isLoading: Boolean) {
+        this.isLoading = isLoading
+        firstButton.setIsLoading(isLoading)
+    }
 
     private fun init(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) {
         LayoutInflater.from(context).inflate(R.layout.screen_feedback, this, true)
@@ -258,6 +265,7 @@ class FeedbackScreenView : ConstraintLayout {
         firstButton = findViewById(R.id.first_button)
         setFeedbackFirstButtonText(firstButtonText)
         firstButtonClickListener?.let { setFirstButtonOnClick(it) }
+        firstButton.setIsLoading(isLoading)
 
         secondButton =
             findViewById(if (secondButtonAsLink) R.id.link_button else R.id.second_button)
