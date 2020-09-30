6194850a75f98b97688a2b8a4c815b385ff355f4
gmerino <gmerino@tuenti.com>
2020-09-30 13:37:44 +0200

    Adding AppCenter to app and deleting it from catalog

---
+++ app/src/main/java/com/telefonica/mistica/app/CatalogApplication.kt
@@ -0,0 +1,27 @@
+package com.telefonica.mistica.app
+
+import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
+import com.telefonica.mistica.catalog.BuildConfig
+
+class CatalogApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initializeAppCenterIfNeeded()
+    }
+
+    private fun initializeAppCenterIfNeeded() {
+        if (!BuildConfig.APPCENTER_KEY.isNullOrEmpty() && BuildConfig.APPCENTER_KEY != "null") {
+            AppCenter.start(
+                this, BuildConfig.APPCENTER_KEY,
+                Distribute::class.java, Analytics::class.java, Crashes::class.java
+            )
+            Distribute.setEnabledForDebuggableBuild(true)
+        }
+    }
+}
