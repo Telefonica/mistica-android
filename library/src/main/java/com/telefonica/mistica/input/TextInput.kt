958b566dddb2bd2c316bdf2a02e1aad66310b17c
Miguel Barrios <miguel.barriosbenito@telefonica.com>
2021-01-26 15:34:35 +0100

    ANDROID-8834 Remove unused import

--- library/src/main/java/com/telefonica/mistica/input/TextInput.kt
+++ library/src/main/java/com/telefonica/mistica/input/TextInput.kt
@@ -7,7 +7,6 @@ import android.text.Editable
 import android.text.InputFilter.LengthFilter
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.EditorInfo
