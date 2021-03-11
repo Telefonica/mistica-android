eb00016a309322eb71ccb9dc10762158619ae922
Jorge Mucientes <jmucientes@tuenti.com>
2021-03-11 16:01:31 +0100

    ANDROID-8984 Allow to customize tag background color

--- library/src/main/java/com/telefonica/mistica/tag/TagView.kt
+++ library/src/main/java/com/telefonica/mistica/tag/TagView.kt
@@ -1,10 +1,15 @@
-package com.telefonica.mistica.tag;
+package com.telefonica.mistica.tag
 
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 import com.google.android.material.textview.MaterialTextView
 import com.telefonica.mistica.R
+import com.telefonica.mistica.util.getThemeColor
+
 
 class TagView @JvmOverloads constructor(
     context: Context,
@@ -14,4 +19,22 @@ class TagView @JvmOverloads constructor(
     ContextThemeWrapper(context, R.style.AppTheme_Widgets_Tag),
     attrs,
     defStyleAttr
-)
+) {
+    init {
+        if (attrs != null) {
+            val styledAttrs =
+                context.theme.obtainStyledAttributes(
+                    attrs,
+                    R.styleable.TagView,
+                    defStyleAttr,
+                    0
+                )
+            styledAttrs.getColor(R.styleable.TagView_tagColor, R.attr.colorPromo)
+                .let {
+                    background.colorFilter = BlendModeColorFilterCompat
+                        .createBlendModeColorFilterCompat(it, BlendModeCompat.SRC_ATOP)
+                }
+            styledAttrs.recycle()
+        }
+    }
+}
