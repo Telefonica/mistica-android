814a4e7af7f2fd17b1e1d29c41f43c71e080058f
Manuel Vera <mvera@tuenti.com>
2021-04-27 16:58:36 +0200

    ANDROID-9281 allow tag color typology - improved catalog

--- catalog/src/main/java/com/telefonica/mistica/catalog/ui/fragment/DataCardFragment.kt
+++ catalog/src/main/java/com/telefonica/mistica/catalog/ui/fragment/DataCardFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.AttrRes
 import androidx.fragment.app.Fragment
 import com.telefonica.mistica.catalog.R
 import com.telefonica.mistica.card.datacard.DataCardView
 import com.telefonica.mistica.input.CheckBoxInput
+import com.telefonica.mistica.input.DropDownInput
 import com.telefonica.mistica.input.TextInput
 import com.telefonica.mistica.util.getThemeColor
 
@@ -25,10 +27,26 @@ class DataCardFragment : Fragment() {
         return inflater.inflate(R.layout.data_card_fragment_catalog, container, false)
     }
 
+    private var currentTagColor: Int = R.attr.colorPromo
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.data_card_button_update)
             .setOnClickListener { updateDataCardView(view) }
+        with(view.findViewById<DropDownInput>(R.id.tagColorSelector).dropDown) {
+            setAdapter(
+                DropDownInput.Adapter(
+                    view.context,
+                    R.layout.dropdown_menu_popup_item,
+                    TagColors.values().map { it.name }
+                )
+            )
+            setText(TagColors.PROMO.name)
+            setOnItemClickListener { _, _, _, _ ->
+                currentTagColor = TagColors.valueOf(text.toString()).colorAttr
+                updateDataCardView(view)
+            }
+        }
         updateDataCardView(view)
     }
 
@@ -40,7 +58,7 @@ class DataCardFragment : Fragment() {
             setDescription(view.findViewById<TextInput>(R.id.input_description).text.toString())
             setPrimaryButtonText(view.findViewById<TextInput>(R.id.input_primary_button).text.toString())
             setLinkButtonText(view.findViewById<TextInput>(R.id.input_link_button).text.toString())
-            setTagColor(context.getThemeColor(R.attr.colorBrand))
+            setTagColor(context.getThemeColor(currentTagColor))
             if (view.findViewById<CheckBoxInput>(R.id.additional_content_checkbox).isChecked()) {
                 setCardAdditionalContent(null)
                 val additionalContent  = LayoutInflater.from(context).inflate(R.layout.card_custom_sample_content, this, false )
@@ -75,5 +93,17 @@ class DataCardFragment : Fragment() {
                 ).show()
             }
         }
+    }
+
+    private enum class TagColors(@AttrRes val colorAttr: Int) {
+        PROMO(R.attr.colorPromo),
+        BRAND(R.attr.colorBrand),
+        BRAND_DARK(R.attr.colorBrandDark),
+        NEUTRAL_HIGH(R.attr.colorNeutralHigh),
+        NEUTRAL_MEDIUM(R.attr.colorNeutralMedium),
+        SUCCESS(R.attr.colorSuccess),
+        WARNING(R.attr.colorWarning),
+        ERROR(R.attr.colorError),
+        INVERSE(R.attr.colorInverse),
     }
 }
