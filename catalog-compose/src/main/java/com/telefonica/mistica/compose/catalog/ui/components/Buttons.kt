a3edc0a799a26849af81e8e02b90901e7759e473
gmerino <guillermo.merino.jimenez@telefonica.com>
2021-10-04 08:58:44 +0200

    ANDROID-9928 - Adding support for Compose catalog (WIP)

--- catalog-compose/src/main/java/com/telefonica/mistica/compose/catalog/ui/components/Buttons.kt
+++ catalog-compose/src/main/java/com/telefonica/mistica/compose/catalog/ui/components/Buttons.kt
@@ -1,9 +1,20 @@
 package com.telefonica.mistica.compose.catalog.ui.components
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.telefonica.mistica.compose.button.Button
 
 @Composable
 fun Buttons() {
-    Text("Buttons!")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+       Button(buttonText = "Button")
+    }
 }
