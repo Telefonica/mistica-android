777cbbea8f605496280776db53e1d199ad368f69
gmerino <guillermo.merino.jimenez@telefonica.com>
2021-11-15 14:47:17 +0100

    ANDROID-10047 - Fixes in dark mode in the catalog

--- catalog-compose/src/main/java/com/telefonica/mistica/compose/catalog/ui/components/Feedbacks.kt
+++ catalog-compose/src/main/java/com/telefonica/mistica/compose/catalog/ui/components/Feedbacks.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -16,12 +15,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.telefonica.mistica.compose.button.Button
 import com.telefonica.mistica.compose.feedback.Feedback
 import com.telefonica.mistica.feedback.screen.view.FeedbackScreenView
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @Composable
 fun Feedbacks(
@@ -71,13 +67,10 @@ fun Feedbacks(
                 Text("Error")
                 Checkbox(checked = type == FeedbackScreenView.TYPE_ERROR, onCheckedChange = { type = FeedbackScreenView.TYPE_ERROR })
             }
-            com.telefonica.mistica.compose.button.Button(
+            Button(
                 text = "Show feedback",
                 onClickListener = { showFeedback = true }
             )
-            Button(onClick = { showFeedback = true }) {
-                Text("Show feedback")
-            }
         }
     }
 }
