81d25cbe38523d51e49e7ad0d4211cfec41f88b5
Manuel Vera <mvera@tuenti.com>
2021-11-23 15:04:11 +0100

    ANDROID-9977 support list row - fix badge text color and add preview

--- library-compose/src/main/java/com/telefonica/mistica/compose/list/Badge.kt
+++ library-compose/src/main/java/com/telefonica/mistica/compose/list/Badge.kt
@@ -1,15 +1,20 @@
 package com.telefonica.mistica.compose.list
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.telefonica.mistica.compose.theme.MisticaTheme
+import com.telefonica.mistica.compose.theme.brand.MovistarBrand
 
 @ExperimentalMaterialApi
 @Composable
@@ -27,11 +32,27 @@ fun Badge(
     } else {
         androidx.compose.material.Badge(
             backgroundColor = MisticaTheme.colors.badge,
+            modifier = modifier,
         ) {
             Text(
                 text = content,
-                color = Color.White,
+                color = MisticaTheme.colors.textPrimaryInverse,
             )
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview(showBackground = true)
+@Composable
+fun BadgePreview() {
+    MisticaTheme(brand = MovistarBrand) {
+        Column(horizontalAlignment = Alignment.End) {
+            Badge(content = "2")
+            Spacer(modifier = Modifier.height(8.dp))
+            Badge(content = "")
+            Spacer(modifier = Modifier.height(8.dp))
+            Badge()
         }
     }
 }
