e094948fbf67e0971774bcca67d6ef39c988c52f
David González <david.gonzalezcolorado@telefonica.com>
2021-12-10 14:48:28 +0100

    ANDROID-10144 Create composable TagView version

--- library-compose/src/main/java/com/telefonica/mistica/compose/tag/Tag.kt
+++ library-compose/src/main/java/com/telefonica/mistica/compose/tag/Tag.kt
@@ -1,65 +1,183 @@
 package com.telefonica.mistica.compose.tag
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.telefonica.mistica.compose.theme.MisticaTheme
-import com.telefonica.mistica.compose.theme.brand.TelefonicaBrand
+import com.telefonica.mistica.compose.theme.brand.MovistarBrand
+import com.telefonica.mistica.tag.TagStyle
+import com.telefonica.mistica.tag.TagView.Companion.TYPE_ACTIVE
+import com.telefonica.mistica.tag.TagView.Companion.TYPE_ERROR
+import com.telefonica.mistica.tag.TagView.Companion.TYPE_INACTIVE
+import com.telefonica.mistica.tag.TagView.Companion.TYPE_INVERSE
+import com.telefonica.mistica.tag.TagView.Companion.TYPE_PROMO
+import com.telefonica.mistica.tag.TagView.Companion.TYPE_SUCCESS
+import com.telefonica.mistica.tag.TagView.Companion.TYPE_WARNING
+import java.util.Locale
 
 @Composable
 fun Tag(
-    content: String,
+    text: String,
     modifier: Modifier = Modifier,
-    color: Color = MisticaTheme.colors.badge,
+    @TagStyle style: Int = TYPE_PROMO,
+    @DrawableRes icon: Int? = null,
 ) {
+    val (background, textColor) = style.getStyle()
+
     Surface(
-        modifier = modifier,
-        shape = RoundedCornerShape(4.dp),
-        color = color,
+        modifier = modifier
+            .wrapContentSize()
+            .defaultMinSize(minWidth = 48.dp)
+            .height(28.dp),
+        shape = RoundedCornerShape(50.dp),
+        color = background,
     ) {
-        Text(
-            text = content.uppercase(),
-            style = MisticaTheme.typography.preset1Medium,
-            color = Color.White,
-            modifier = Modifier.padding(
-                horizontal = 8.dp,
-            ),
-        )
+        Row(
+            modifier = Modifier.padding(start = if (icon != null) 8.dp else 12.dp, end = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+            if (icon != null) {
+                Image(
+                    painter = painterResource(id = icon),
+                    contentDescription = null,
+                    modifier = Modifier.size(16.dp),
+                    colorFilter = ColorFilter.tint(textColor),
+                    contentScale = ContentScale.Fit
+                )
+            }
+
+            Text(
+                text = text.captitalizeFirstChar(),
+                modifier = Modifier.padding(start = if (icon != null) 4.dp else 0.dp),
+                style = MisticaTheme.typography.preset2Medium,
+                color = textColor,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
     }
 }
 
+class Tag constructor(
+    private val content: String,
+) {
+    private var modifier: Modifier = Modifier
+
+    @TagStyle
+    private var style: Int = TYPE_PROMO
+
+    @DrawableRes
+    private var icon: Int? = null
+
+    fun withModifier(modifier: Modifier) = apply { this.modifier = modifier }
+    fun withStyle(@TagStyle style: Int) = apply { this.style = style }
+    fun withIcon(@DrawableRes icon: Int?) = apply { this.icon = icon }
+
+    @Composable
+    fun build() {
+        Tag(text = content, modifier = modifier, style = style, icon = icon)
+    }
+}
+
+@ExperimentalFoundationApi
 @Preview(showBackground = true)
 @Composable
 fun TagPreview() {
-    MisticaTheme(brand = TelefonicaBrand) {
-        Column {
-            Box(
-                modifier = Modifier
-                    .size(150.dp, 100.dp)
-                    .background(MisticaTheme.colors.backgroundContainer),
-            ) {
-                Tag(
-                    content = "new",
-                    color = MisticaTheme.colors.promo,
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .shadow(
-                            elevation = 2.dp,
-                            shape = RoundedCornerShape(4.dp)
-                        ),
-                )
-            }
+    MisticaTheme(brand = MovistarBrand) {
+        Column(
+            modifier = Modifier
+                .padding(vertical = 32.dp, horizontal = 16.dp)
+                .background(MisticaTheme.colors.backgroundContainer),
+        ) {
+            TagCatalogPreview()
         }
     }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun TagCatalogPreview() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Surface(
+            color = MisticaTheme.colors.brand,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+        ) {
+            Tag(text = "Inverse", style = TYPE_INVERSE, modifier = Modifier.padding(8.dp))
+        }
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(3),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            item { Tag(text = "Promotion", style = TYPE_PROMO, modifier = Modifier.padding(4.dp)) }
+            item { Tag(text = "Active", style = TYPE_ACTIVE, modifier = Modifier.padding(4.dp)) }
+            item { Tag(text = "Inactive", style = TYPE_INACTIVE, modifier = Modifier.padding(4.dp)) }
+            item { Tag(text = "Success", style = TYPE_SUCCESS, modifier = Modifier.padding(4.dp)) }
+            item { Tag(text = "Warning", style = TYPE_WARNING, modifier = Modifier.padding(4.dp)) }
+            item { Tag(text = "Error", style = TYPE_ERROR, modifier = Modifier.padding(4.dp)) }
+        }
+        Surface(
+            color = MisticaTheme.colors.brand,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+                .height(60.dp)
+        ) {
+            Tag(text = "Inverse", style = TYPE_INVERSE, modifier = Modifier.padding(8.dp), icon = android.R.drawable.ic_lock_power_off)
+        }
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(3),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            item { Tag(text = "Promotion", style = TYPE_PROMO, modifier = Modifier.padding(4.dp), icon = android.R.drawable.ic_lock_power_off) }
+            item { Tag(text = "Active", style = TYPE_ACTIVE, modifier = Modifier.padding(4.dp), icon = android.R.drawable.ic_lock_power_off) }
+            item { Tag(text = "Inactive", style = TYPE_INACTIVE, modifier = Modifier.padding(4.dp), icon = android.R.drawable.ic_lock_power_off) }
+            item { Tag(text = "Success", style = TYPE_SUCCESS, modifier = Modifier.padding(4.dp), icon = android.R.drawable.ic_lock_power_off) }
+            item { Tag(text = "Warning", style = TYPE_WARNING, modifier = Modifier.padding(4.dp), icon = android.R.drawable.ic_lock_power_off) }
+            item { Tag(text = "Error", style = TYPE_ERROR, modifier = Modifier.padding(4.dp), icon = android.R.drawable.ic_lock_power_off) }
+        }
+    }
+}
+
+@Composable
+private fun Int.getStyle() = when (this) {
+    TYPE_PROMO -> with(MisticaTheme.colors) { tagBackgroundPromo to textTagPromo }
+    TYPE_ACTIVE -> with(MisticaTheme.colors) { tagBackgroundActive to textTagActive }
+    TYPE_INACTIVE -> with(MisticaTheme.colors) { tagBackgroundInactive to textTagInactive }
+    TYPE_SUCCESS -> with(MisticaTheme.colors) { tagBackgroundSuccess to textTagSuccess }
+    TYPE_WARNING -> with(MisticaTheme.colors) { tagBackgroundWarning to textTagWarning }
+    TYPE_ERROR -> with(MisticaTheme.colors) { tagBackgroundError to textTagError }
+    TYPE_INVERSE -> with(MisticaTheme.colors) { inverse to textTagActive }
+    else -> with(MisticaTheme.colors) { tagBackgroundPromo to textTagPromo }
+}
+
+private fun String.captitalizeFirstChar(): String {
+    return lowercase(Locale.getDefault()).replaceFirstChar { if (it.isLowerCase()) it.uppercase() else it.toString() }
 }
