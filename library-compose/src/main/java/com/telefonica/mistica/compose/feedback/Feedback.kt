3a4f866f768c55bb2a062d9f73c077edb531857e
yamal <yamal@tuenti.com>
2022-05-27 11:26:08 +0200

    ANDROID-10695 Add modifier to Feedback component

--- library-compose/src/main/java/com/telefonica/mistica/compose/feedback/Feedback.kt
+++ library-compose/src/main/java/com/telefonica/mistica/compose/feedback/Feedback.kt
@@ -7,11 +7,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.telefonica.mistica.feedback.screen.view.FeedbackScreenView
 
 @Composable
 fun Feedback(
+    modifier: Modifier = Modifier,
     @FeedbackScreenView.FeedbackType type: Int = FeedbackScreenView.TYPE_INFO,
     title: String = "",
     subtitle: String = "",
@@ -30,27 +32,30 @@ fun Feedback(
         BackHandler(onBack = onBackPressed)
     }
 
-    AndroidView(factory = { context ->
-        FeedbackScreenView(context).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            shouldAnimateOnAttachedToWindow = false
-            setFeedbackType(type)
-            setFeedbackTitle(title)
-            setFeedbackSubtitle(subtitle)
-            setFeedbackErrorReference(errorReference)
-            firstButtonText?.let { setFeedbackFirstButtonText(firstButtonText) }
-            secondButtonText?.let { setFeedbackSecondButtonText(secondButtonText) }
-            firstButtonOnClick?.let { setFirstButtonOnClick { firstButtonOnClick() } }
-            secondButtonOnClick?.let { setSecondButtonOnClick { secondButtonOnClick() } }
-            setFeedbackSecondButtonAsLink(secondButtonAsLink)
-            setIsLoading(isFirstButtonLoading)
-            if (shouldPerformTheAnimation) {
-                animateViews()
-                shouldPerformTheAnimation = false
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            FeedbackScreenView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                shouldAnimateOnAttachedToWindow = false
+                setFeedbackType(type)
+                setFeedbackTitle(title)
+                setFeedbackSubtitle(subtitle)
+                setFeedbackErrorReference(errorReference)
+                firstButtonText?.let { setFeedbackFirstButtonText(firstButtonText) }
+                secondButtonText?.let { setFeedbackSecondButtonText(secondButtonText) }
+                firstButtonOnClick?.let { setFirstButtonOnClick { firstButtonOnClick() } }
+                secondButtonOnClick?.let { setSecondButtonOnClick { secondButtonOnClick() } }
+                setFeedbackSecondButtonAsLink(secondButtonAsLink)
+                setIsLoading(isFirstButtonLoading)
+                if (shouldPerformTheAnimation) {
+                    animateViews()
+                    shouldPerformTheAnimation = false
+                }
             }
         }
-    })
+    )
 
 }
