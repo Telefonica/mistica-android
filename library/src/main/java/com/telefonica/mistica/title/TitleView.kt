77a58deaf251cba88e6c127e43ebdd0138af7506
yamal <yamal@tuenti.com>
2022-06-15 13:59:06 +0200

    ANDROID-10654 Allow different Title styles

--- library/src/main/java/com/telefonica/mistica/title/TitleView.kt
+++ library/src/main/java/com/telefonica/mistica/title/TitleView.kt
@@ -4,17 +4,27 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.TextView
+import androidx.annotation.AttrRes
+import androidx.annotation.IntDef
+import androidx.annotation.StyleRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingMethod
 import androidx.databinding.BindingMethods
 import com.telefonica.mistica.R
+import com.telefonica.mistica.util.getThemeColor
 import com.telefonica.mistica.util.setTextAndVisibility
+import com.telefonica.mistica.util.setTextPreset
 
 @BindingMethods(
     BindingMethod(
         type = TitleView::class,
         attribute = "title",
         method = "setTitle"
+    ),
+    BindingMethod(
+        type = TitleView::class,
+        attribute = "titleStyle",
+        method = "setTitleStyle"
     ),
     BindingMethod(
         type = TitleView::class,
@@ -32,6 +42,13 @@ class TitleView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    @Retention(AnnotationRetention.SOURCE)
+    @IntDef(
+        TITLE_1,
+        TITLE_2,
+    )
+    annotation class TitleStyle
 
     private var lintTextView: TextView
     private var titleTextView: TextView
@@ -52,6 +69,8 @@ class TitleView @JvmOverloads constructor(
                 )
 
             titleTextView.setTextAndVisibility(styledAttrs.getText(R.styleable.TitleView_title))
+            setTitleStyle(styledAttrs.getInt(R.styleable.TitleView_titleStyle, -1))
+
             lintTextView.setTextAndVisibility(styledAttrs.getText(R.styleable.TitleView_link))
 
             styledAttrs.recycle()
@@ -62,6 +81,18 @@ class TitleView @JvmOverloads constructor(
         titleTextView.setTextAndVisibility(text)
     }
 
+    fun setTitleStyle(@TitleStyle style: Int) {
+        val styleConfig = when (style) {
+            TITLE_1 -> TITLE_1_STYLE_CONFIG
+            TITLE_2 -> TITLE_2_STYLE_CONFIG
+            else -> TITLE_2_STYLE_CONFIG
+        }
+
+        titleTextView.setTextPreset(styleConfig.preset)
+        titleTextView.setTextColor(context.getThemeColor(styleConfig.colorAttrRes))
+        titleTextView.isAllCaps = styleConfig.isAllCaps
+    }
+
     fun setLink(text: CharSequence?) {
         lintTextView.setTextAndVisibility(text)
     }
@@ -70,4 +101,26 @@ class TitleView @JvmOverloads constructor(
         lintTextView.setOnClickListener { listener.invoke() }
     }
 
+    companion object {
+        const val TITLE_1 = 0
+        const val TITLE_2 = 1
+
+        private val TITLE_1_STYLE_CONFIG = TitleStyleConfig(
+            preset = R.style.AppTheme_TextAppearance_Preset5,
+            colorAttrRes = R.attr.colorTextPrimary,
+            isAllCaps = false,
+        )
+
+        private val TITLE_2_STYLE_CONFIG = TitleStyleConfig(
+            preset = R.style.AppTheme_TextAppearance_Preset1,
+            colorAttrRes = R.attr.colorTextSecondary,
+            isAllCaps = true,
+        )
+    }
 }
+
+private data class TitleStyleConfig(
+    @StyleRes val preset: Int,
+    @AttrRes val colorAttrRes: Int,
+    val isAllCaps: Boolean,
+)
