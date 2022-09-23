c5ab3a9e9f44c22ae34afabe04ca682dbc859ba5
gmerino <guillermo.merino.jimenez@telefonica.com>
2022-09-23 13:55:56 +0200

    ANDROID-11118 Sorting

--- library/src/main/java/com/telefonica/mistica/bottomsheet/children/list/SelectableListAdapter.kt
+++ library/src/main/java/com/telefonica/mistica/bottomsheet/children/list/SelectableListAdapter.kt
@@ -75,11 +75,11 @@ private fun ListRowView.setAsset(asset: AssetViewData?) {
             this.setAssetType(TYPE_SMALL_ICON)
             this.setAssetResource(asset.id)
         }
-        null -> {}
         is AssetViewData.SmallImage -> {
             this.setAssetType(TYPE_SMALL_ICON)
             this.setAssetDrawable(asset.drawableRes)
         }
+        null -> {}
     }
 }
 
