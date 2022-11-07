1fac752f308f89a141298d5bf5adb9615449c22b
Javier Delgado <javier.delgadoaylagas@telefonica.com>
2022-11-07 10:39:06 +0100

    Remove deprecations

--- catalog-compose/src/main/java/com/telefonica/mistica/compose/catalog/ui/components/Lists.kt
+++ catalog-compose/src/main/java/com/telefonica/mistica/compose/catalog/ui/components/Lists.kt
@@ -16,9 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
 import com.telefonica.mistica.compose.catalog.R
 import com.telefonica.mistica.compose.list.BackgroundType
@@ -273,11 +276,11 @@ fun Avatar() {
 @Composable
 fun Avatar(url: String) {
     Image(
-        painter = rememberImagePainter(
-            data = url,
-            builder = {
-                transformations(CircleCropTransformation())
-            }
+        painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(LocalContext.current)
+                .data(url)
+                .apply { transformations(CircleCropTransformation()) }
+                .build()
         ),
         contentDescription = null,
         modifier = Modifier.size(40.dp)
