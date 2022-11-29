445ea17683ee97690de3f4d87612c70308e8924b
yamal <yamal@tuenti.com>
2022-11-29 17:30:56 +0100

    ANDROID-11418 Compose version of Callout

---
+++ catalog-compose/src/main/java/com/telefonica/mistica/compose/catalog/ui/components/Callouts.kt
@@ -0,0 +1,156 @@
+package com.telefonica.mistica.compose.catalog.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Checkbox
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.telefonica.mistica.compose.button.Button
+import com.telefonica.mistica.compose.callout.Callout
+import com.telefonica.mistica.compose.callout.CalloutButtonConfig
+import com.telefonica.mistica.compose.catalog.R
+import com.telefonica.mistica.compose.catalog.ui.common.DropDown
+import com.telefonica.mistica.compose.theme.MisticaTheme
+
+@Composable
+fun Callouts() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        var inverse by remember { mutableStateOf(false) }
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(checked = inverse, onCheckedChange = { inverse = !inverse })
+            Text("Inverse variant")
+        }
+
+        var showIcon by remember { mutableStateOf(false) }
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(checked = showIcon, onCheckedChange = { showIcon = !showIcon })
+            Text("Show icon")
+        }
+
+        var dismissable by remember { mutableStateOf(false) }
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(checked = dismissable, onCheckedChange = { dismissable = !dismissable })
+            Text("Is dismissable")
+        }
+
+        var title by remember { mutableStateOf("Callout sample title") }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            value = title,
+            onValueChange = { title = it },
+            label = { Text("Title") }
+        )
+
+        var description by remember { mutableStateOf("Callout sample description") }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            value = description,
+            onValueChange = { description = it },
+            label = { Text("Description") }
+        )
+
+        var buttonConfig by remember { mutableStateOf(CalloutButtonConfig.PRIMARY) }
+        DropDown(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            selectedValue = buttonConfig,
+            onValueChanged = { buttonConfig = it }
+        )
+
+        var primaryButtonText by remember { mutableStateOf("Primary Action") }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            value = primaryButtonText,
+            onValueChange = { primaryButtonText = it },
+            label = { Text("Primary Button text") }
+        )
+
+        var secondaryButtonText by remember { mutableStateOf("Secondary Action") }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            value = secondaryButtonText,
+            onValueChange = { secondaryButtonText = it },
+            label = { Text("Secondary Button text") }
+        )
+
+        var linkText by remember { mutableStateOf("Link Action") }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            value = linkText,
+            onValueChange = { linkText = it },
+            label = { Text("Link Button text") }
+        )
+
+        var isShown by remember { mutableStateOf(true) }
+
+        if (isShown) {
+            Column(
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth()
+                    .background(
+                        if (inverse) {
+                            MisticaTheme.colors.backgroundBrand
+                        } else {
+                            MisticaTheme.colors.backgroundContainer
+                        }
+                    ),
+            ) {
+                AnimatedVisibility(visible = isShown) {
+                    Callout(
+                        modifier = Modifier.fillMaxWidth().padding(16.dp),
+                        title = title.takeIf { it.isNotBlank() },
+                        description = description.takeIf { it.isNotBlank() },
+                        buttonConfig = buttonConfig,
+                        iconRes = if (showIcon) R.drawable.ic_callout else null,
+                        dismissable = dismissable,
+                        inverse = inverse,
+                        onDismiss = { isShown = false },
+                        primaryButtonText = primaryButtonText.takeIf { it.isNotBlank() },
+                        secondaryButtonText = secondaryButtonText.takeIf { it.isNotBlank() },
+                        linkText = linkText.takeIf { it.isNotBlank() },
+                    )
+                }
+            }
+        } else {
+            Button(
+                modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp, top = 8.dp),
+                text = "Show callout again",
+                onClickListener = {
+                    isShown = true
+                }
+            )
+        }
+    }
+}
+
---
+++ library-compose/src/main/java/com/telefonica/mistica/compose/callout/Callout.kt
@@ -0,0 +1,175 @@
+package com.telefonica.mistica.compose.callout
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.telefonica.mistica.compose.R
+import com.telefonica.mistica.compose.button.Button
+import com.telefonica.mistica.compose.button.ButtonStyle
+import com.telefonica.mistica.compose.theme.MisticaTheme
+
+@Composable
+fun Callout(
+    modifier: Modifier = Modifier,
+    title: String?,
+    description: String?,
+    buttonConfig: CalloutButtonConfig,
+    @DrawableRes iconRes: Int?,
+    dismissable: Boolean,
+    onDismiss: (() -> Unit)? = null,
+    inverse: Boolean,
+    primaryButtonText: String? = null,
+    secondaryButtonText: String? = null,
+    onPrimaryButtonClick: (() -> Unit)? = null,
+    onSecondaryButtonClick: (() -> Unit)? = null,
+    linkText: String? = null,
+    onLinkClicked: (() -> Unit)? = null,
+) {
+    Box(
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(8.dp))
+            .background(
+                if (inverse) {
+                    MisticaTheme.colors.backgroundContainer
+                } else {
+                    MisticaTheme.colors.backgroundAlternative
+                }
+            )
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.Top,
+        ) {
+            iconRes?.let {
+                Image(
+                    modifier = Modifier.padding(end = 16.dp),
+                    painter = painterResource(id = iconRes),
+                    contentDescription = null
+                )
+            }
+
+            Column(
+                modifier = Modifier.weight(1F)
+            ) {
+                title?.let {
+                    Text(
+                        text = it,
+                        style = MisticaTheme.typography.preset3,
+                    )
+                }
+                description?.let {
+                    Text(
+                        text = it,
+                        style = MisticaTheme.typography.preset2,
+                        color = MisticaTheme.colors.textSecondary
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                ) {
+                    when (buttonConfig) {
+                        CalloutButtonConfig.NONE -> { /* No buttons are shown */ }
+                        CalloutButtonConfig.PRIMARY -> {
+                            PrimaryButton(primaryButtonText, onPrimaryButtonClick)
+                        }
+                        CalloutButtonConfig.PRIMARY_AND_LINK -> {
+                            PrimaryButton(primaryButtonText, onPrimaryButtonClick)
+                            LinkButton(linkText, onLinkClicked)
+                        }
+                        CalloutButtonConfig.SECONDARY -> {
+                            SecondaryButton(secondaryButtonText, onSecondaryButtonClick)
+                        }
+                        CalloutButtonConfig.PRIMARY_AND_SECONDARY -> {
+                            PrimaryButton(primaryButtonText, onPrimaryButtonClick)
+                            SecondaryButton(secondaryButtonText, onSecondaryButtonClick)
+                        }
+                        CalloutButtonConfig.SECONDARY_AND_LINK -> {
+                            SecondaryButton(secondaryButtonText, onSecondaryButtonClick)
+                            LinkButton(linkText, onLinkClicked)
+                        }
+                        CalloutButtonConfig.LINK -> {
+                            LinkButton(linkText, onLinkClicked)
+                        }
+                    }
+                }
+            }
+
+            if (dismissable) {
+                Image(
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .clickable { onDismiss?.invoke() },
+                    painter = painterResource(id = R.drawable.icn_cross),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun PrimaryButton(
+    text: String?,
+    onClick: (() -> Unit)? = null
+) {
+    CalloutButton(text, onClick, ButtonStyle.PRIMARY_SMALL)
+}
+
+@Composable
+private fun SecondaryButton(
+    text: String?,
+    onClick: (() -> Unit)? = null
+) {
+    CalloutButton(text, onClick, ButtonStyle.SECONDARY_SMALL)
+}
+
+@Composable
+private fun LinkButton(
+    text: String?,
+    onClick: (() -> Unit)? = null
+) {
+    CalloutButton(text, onClick, ButtonStyle.LINK)
+}
+
+@Composable
+private fun CalloutButton(
+    text: String?,
+    onClick: (() -> Unit)?,
+    style: ButtonStyle,
+) {
+    text?.let {
+        Button(
+            modifier = Modifier.padding(end = 16.dp),
+            text = text,
+            buttonStyle = style,
+            onClickListener = { onClick?.invoke() }
+        )
+    }
+}
+
+enum class CalloutButtonConfig {
+    NONE,
+    PRIMARY,
+    PRIMARY_AND_LINK,
+    PRIMARY_AND_SECONDARY,
+    SECONDARY,
+    SECONDARY_AND_LINK,
+    LINK,
+}
