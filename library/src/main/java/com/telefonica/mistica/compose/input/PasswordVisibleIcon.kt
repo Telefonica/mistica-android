7eb431bc19e2de00b0e86d585d76825236271d78
Josep Rubio <jose.rubiomorente@telefonica.com>
2023-04-03 16:41:22 +0200

    ANDROID-11907 Tests in roboelectric

--- library/src/main/java/com/telefonica/mistica/compose/input/PasswordVisibleIcon.kt
+++ library/src/main/java/com/telefonica/mistica/compose/input/PasswordVisibleIcon.kt
@@ -3,8 +3,12 @@ package com.telefonica.mistica.compose.input
 import androidx.compose.material.Icon
 import androidx.compose.material.IconToggleButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import com.telefonica.mistica.R
+
+internal const val TOGGLE_VISIBILITY_TEST_TAG = "ToggleVisibility"
 
 @Composable
 fun PasswordVisibleIcon(
@@ -13,7 +17,11 @@ fun PasswordVisibleIcon(
     showContentDescription: String? = null,
     hideContentDescription: String? = null,
 ) {
-    IconToggleButton(checked = passwordVisible, onCheckedChange = onIconClicked) {
+    IconToggleButton(
+        checked = passwordVisible,
+        onCheckedChange = onIconClicked,
+        modifier = Modifier.testTag(TOGGLE_VISIBILITY_TEST_TAG),
+    ) {
         Icon(
             painter = if (passwordVisible) {
                 painterResource(id = R.drawable.icn_visibility_off)
