185c540e2932e675f54389a1f2a355bd3ac27f3c
Josep Rubio <jose.rubiomorente@telefonica.com>
2023-04-04 16:34:51 +0200

    ANDROID-11907 Add previews for empty PreviewPasswordVisibleIcon

--- library/src/main/java/com/telefonica/mistica/compose/input/PasswordVisibleIcon.kt
+++ library/src/main/java/com/telefonica/mistica/compose/input/PasswordVisibleIcon.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.telefonica.mistica.R
 
 internal const val TOGGLE_VISIBILITY_TEST_TAG = "ToggleVisibility"
@@ -31,4 +32,22 @@ fun PasswordVisibleIcon(
             contentDescription = if (passwordVisible) hideContentDescription else showContentDescription,
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPasswordVisibleIconVisible() {
+    PasswordVisibleIcon(
+        passwordVisible = true,
+        onIconClicked = {},
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPasswordVisibleIconInVisible() {
+    PasswordVisibleIcon(
+        passwordVisible = false,
+        onIconClicked = {},
+    )
 }
