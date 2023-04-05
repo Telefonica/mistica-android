baeaf6e099b28b51181d4d791d4d486b9b48d0d3
Josep Rubio <jose.rubiomorente@telefonica.com>
2023-04-05 17:59:23 +0200

    ANDROID-11916 Update README

--- library/src/main/java/com/telefonica/mistica/compose/input/TextInputImpl.kt
+++ library/src/main/java/com/telefonica/mistica/compose/input/TextInputImpl.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
