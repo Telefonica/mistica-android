c847b8350f64380323e10c718378593b52323aa2
Javier Delgado <javier.delgadoaylagas@telefonica.com>
2023-04-11 11:42:27 +0200

    ANDROID-11501 load error feedback compose (#225)
    * ANDROID-11501 load error feedback compose

---
+++ library/src/main/java/com/telefonica/mistica/compose/feedback/error/LoadErrorFeedback.kt
@@ -0,0 +1,89 @@
+package com.telefonica.mistica.compose.feedback.error
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.telefonica.mistica.compose.button.Button
+import com.telefonica.mistica.compose.button.ButtonStyle
+import com.telefonica.mistica.compose.theme.MisticaTheme
+import com.telefonica.mistica.compose.theme.brand.MovistarBrand
+
+@Composable
+fun LoadErrorFeedback(
+    titleText: String? = null,
+    descriptionText: String? = null,
+    isLoading: Boolean = false,
+    buttonText: String? = null,
+    buttonLoadingText: String = "",
+    onButtonClick: () -> Unit,
+) {
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        titleText?.let {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = it,
+                textAlign = TextAlign.Center,
+                style = MisticaTheme.typography.preset4Light,
+                color = MisticaTheme.colors.textPrimary
+            )
+        }
+        descriptionText?.let {
+            Text(
+                text = it,
+                textAlign = TextAlign.Center,
+                style = MisticaTheme.typography.preset2,
+                color = MisticaTheme.colors.textSecondary
+            )
+        }
+        buttonText?.let {
+            Button(
+                modifier = Modifier.padding(top = 24.dp),
+                text = it,
+                loadingText = buttonLoadingText,
+                onClickListener = onButtonClick,
+                isLoading = isLoading,
+                buttonStyle = ButtonStyle.SECONDARY
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun LoadErrorFeedbackPreview() {
+    MisticaTheme(brand = MovistarBrand) {
+        LoadErrorFeedback(
+            titleText = "Title",
+            descriptionText = "Something went wrong, this is the description of the failure. Try to retry if you want.",
+            buttonText = "Button text",
+            buttonLoadingText = "Loading"
+        ) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoadErrorFeedbackNoButtonPreview() {
+    MisticaTheme(brand = MovistarBrand) {
+        LoadErrorFeedback(
+            titleText = "Title",
+            descriptionText = "Something went wrong, this is the description of the failure. Try to retry if you want.",
+        ) {
+
+        }
+    }
+}
+
