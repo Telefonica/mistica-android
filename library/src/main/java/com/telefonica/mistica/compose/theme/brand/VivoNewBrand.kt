7eb57f7c636983dc296a0e4f4af32638ecb714c8
Jesús Latorre <jesus.latorreasensio@tuenti.com>
2023-06-05 15:23:30 +0200

    ANDROID-13323 Create new vivo skin

---
+++ library/src/main/java/com/telefonica/mistica/compose/theme/brand/VivoNewBrand.kt
@@ -0,0 +1,23 @@
+package com.telefonica.mistica.compose.theme.brand
+
+import com.telefonica.mistica.R
+
+object VivoNewBrand : Brand {
+
+    override val compatibilityTheme: Int
+        get() = R.style.MisticaTheme_VivoNew
+
+    override val lightColors = VivoNewBrandColors.lightColors
+
+    override val darkColors = VivoNewBrandColors.darkColors
+
+    override val preset5FontWeight = VivoNewBrandFontWeights.text5FontWeight
+
+    override val preset6FontWeight = VivoNewBrandFontWeights.text6FontWeight
+
+    override val preset7FontWeight = VivoNewBrandFontWeights.text7FontWeight
+
+    override val preset8FontWeight = VivoNewBrandFontWeights.text8FontWeight
+
+    override val radius = VivoNewBrandRadius.radius
+}
---
+++ tokens/src/main/java/com/telefonica/mistica/tokens/Brand.kt
@@ -0,0 +1,6 @@
+package com.telefonica.mistica.tokens
+
+data class Brand(
+    val name: String,
+    val file: String,
+)
