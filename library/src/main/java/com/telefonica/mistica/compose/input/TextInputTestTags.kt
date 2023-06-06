50ea5e1f433a9e8abe59cfe11e5615a9c60c7719
yamal <yamal@tuenti.com>
2023-06-06 15:30:59 +0200

    rename testTag

--- library/src/main/java/com/telefonica/mistica/compose/input/TextInputTestTags.kt
+++ library/src/main/java/com/telefonica/mistica/compose/input/TextInputTestTags.kt
@@ -2,5 +2,5 @@ package com.telefonica.mistica.compose.input
 
 object TextInputTestTags {
     const val TEXT_INPUT = "text_input"
-    const val PASSWORD_VISIBILITY_TOGGLE = "password_toggle_visibility"
+    const val PASSWORD_VISIBILITY_TOGGLE = "password_visibility_toggle"
 }
