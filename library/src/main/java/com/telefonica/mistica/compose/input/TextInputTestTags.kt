0c2e0344e575fdf5b7b90330c9dd9b5869c8c91d
yamal <yamal@tuenti.com>
2023-06-06 15:28:47 +0200

    expose password toggle test tag

--- library/src/main/java/com/telefonica/mistica/compose/input/TextInputTestTags.kt
+++ library/src/main/java/com/telefonica/mistica/compose/input/TextInputTestTags.kt
@@ -2,4 +2,5 @@ package com.telefonica.mistica.compose.input
 
 object TextInputTestTags {
     const val TEXT_INPUT = "text_input"
+    const val PASSWORD_VISIBILITY_TOGGLE = "password_toggle_visibility"
 }
