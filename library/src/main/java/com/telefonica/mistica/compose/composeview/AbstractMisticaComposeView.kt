acaa59c60aca3ea2fc5cebad1eb1670bae8710eb
Pablo Garcia <pablo.garcianunez@telefonica.com>
2023-07-28 11:47:44 +0200

    Unify Ids Tag

--- library/src/main/java/com/telefonica/mistica/compose/composeview/AbstractMisticaComposeView.kt
+++ library/src/main/java/com/telefonica/mistica/compose/composeview/AbstractMisticaComposeView.kt
@@ -2,9 +2,15 @@ package com.telefonica.mistica.compose.composeview
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.compose.material.Surface
 import androidx.annotation.IntDef
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.AbstractComposeView
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import com.telefonica.mistica.R
 import com.telefonica.mistica.compose.composeview.AbstractMisticaComposeView.Companion.BRAND_VALUE_BLAU
 import com.telefonica.mistica.compose.composeview.AbstractMisticaComposeView.Companion.BRAND_VALUE_MOVISTAR
@@ -53,10 +59,22 @@ abstract class AbstractMisticaComposeView @JvmOverloads constructor(
         }
     }
 
+    @OptIn(ExperimentalComposeUiApi::class)
     @Composable
-    open fun Theme(brand: Brand = calculateBrand(), body: @Composable () -> Unit) {
+    open fun Theme(
+        brand: Brand = calculateBrand(),
+        testTagsAsResourceId: Boolean = true,
+        body: @Composable () -> Unit,
+    ) {
         MisticaTheme(brand) {
-            body()
+            Surface(
+                color = Color.Transparent,
+                modifier = Modifier
+                    .semantics {
+                this.testTagsAsResourceId = testTagsAsResourceId
+            }) {
+                body()
+            }
         }
     }
 
