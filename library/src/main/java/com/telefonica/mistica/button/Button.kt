3dd0f4d84d8b3123d36877dab855f2bf237ec452
Josep Rubio <jose.rubiomorente@telefonica.com>
2023-08-04 11:48:46 +0200

    ANDROID-13479 Add chevron to Button in xml

--- library/src/main/java/com/telefonica/mistica/button/Button.kt
+++ library/src/main/java/com/telefonica/mistica/button/Button.kt
@@ -2,23 +2,36 @@ package com.telefonica.mistica.button
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.core.content.ContextCompat
 import com.google.android.material.button.MaterialButton
+import com.telefonica.mistica.R
 import com.telefonica.mistica.util.setAlpha
 
-class Button : MaterialButton {
-
-    constructor(context: Context) : super(context)
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
+class Button @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : MaterialButton(context, attrs, defStyleAttr) {
 
     override fun setEnabled(enabled: Boolean) {
         super.setEnabled(enabled)
         setAlpha(enabled)
+    }
+
+    init {
+        val styledAttrs =
+            context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.Button,
+                0,
+                0,
+            )
+        val hasChevron = styledAttrs.getBoolean(R.styleable.Button_hasChevron, false)
+        if (hasChevron) {
+            val chevronImage = ContextCompat.getDrawable(context, R.drawable.icn_chevron)
+            chevronImage?.setTint(currentTextColor)
+            compoundDrawablePadding = resources.getDimensionPixelSize(R.dimen.button_chevron_padding)
+            this.setCompoundDrawablesWithIntrinsicBounds(null, null, chevronImage, null)
+        }
     }
 }
