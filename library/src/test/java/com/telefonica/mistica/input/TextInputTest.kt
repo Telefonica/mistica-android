b217f04e9946c99f2ddf77e5e2445e2517d07d7e
Josep Rubio <jose.rubiomorente@telefonica.com>
2023-10-13 09:49:23 +0200

    ANDROID-13796 remove unneeded code

--- library/src/test/java/com/telefonica/mistica/input/TextInputTest.kt
+++ library/src/test/java/com/telefonica/mistica/input/TextInputTest.kt
@@ -7,23 +7,15 @@ import androidx.test.ext.junit.rules.activityScenarioRule
 import com.telefonica.mistica.DummyActivity
 import com.telefonica.mistica.R
 import com.telefonica.mistica.testutils.ScreenshotsTest
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.shadows.ShadowLog
 
 @RunWith(RobolectricTestRunner::class)
 internal class TextInputTest: ScreenshotsTest() {
     @get:Rule
     val rule = activityScenarioRule<DummyActivity>()
-
-    @Before
-    @Throws(Exception::class)
-    fun setUp() {
-        ShadowLog.stream = System.out
-    }
 
     @Test
     fun `check TextInput`() {
