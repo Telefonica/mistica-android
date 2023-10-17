95c4b36c77092d704af743ca2531dc4192507daa
yamal <yamal@tuenti.com>
2023-10-17 12:24:01 +0200

    ANDROID-13817 Place action in another line if it is too long

--- library/src/main/java/com/telefonica/mistica/feedback/snackbar/CustomSnackbarLayout.kt
+++ library/src/main/java/com/telefonica/mistica/feedback/snackbar/CustomSnackbarLayout.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.TextView
 import androidx.annotation.ColorInt
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import com.telefonica.mistica.R
 
 class CustomSnackbarLayout @JvmOverloads constructor(
@@ -14,8 +15,13 @@ class CustomSnackbarLayout @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
 ): ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val maxActionLengthPx: Int
+
     init {
         LayoutInflater.from(context).inflate(R.layout.snackbar_custom_layout_merge, this, true)
+
+        maxActionLengthPx = context.resources.getDimensionPixelSize(R.dimen.mistica_snackbar_maxActionInlineWidth)
     }
 
     fun setText(text: CharSequence) {
@@ -39,4 +45,34 @@ class CustomSnackbarLayout @JvmOverloads constructor(
 
     private fun getAction(): TextView =
         findViewById(R.id.custom_snackbar_action)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        if (actionIsTooLong()) {
+            rearrangeLayout()
+        }
+    }
+
+    private fun actionIsTooLong(): Boolean =
+        getAction().measuredWidth > maxActionLengthPx
+
+    private fun rearrangeLayout() {
+        val text = getText()
+        val action = getAction()
+        val parent = this
+
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(parent)
+
+        constraintSet.connect(text.id, ConstraintSet.END, parent.id, ConstraintSet.END)
+        constraintSet.connect(text.id, ConstraintSet.BOTTOM, action.id, ConstraintSet.TOP)
+
+        constraintSet.connect(action.id, ConstraintSet.START, parent.id, ConstraintSet.START)
+        constraintSet.connect(action.id, ConstraintSet.BOTTOM, parent.id, ConstraintSet.BOTTOM)
+        constraintSet.connect(action.id, ConstraintSet.TOP, text.id, ConstraintSet.BOTTOM)
+        constraintSet.setHorizontalBias(action.id, 1.0F)
+
+        constraintSet.applyTo(this)
+    }
 }
