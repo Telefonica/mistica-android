facc07c8ed55ae2ddc6fbbc6b58a947cfbfd8887
Pablo Martín <pablo.martinblanco@telefonica.com>
2023-11-20 11:59:47 +0100

    ANDROID-13871 Add screenshot tests for all types of buttons (#308)

--- library/src/test/java/com/telefonica/mistica/testutils/TestUtils.kt
+++ library/src/test/java/com/telefonica/mistica/testutils/TestUtils.kt
@@ -1,5 +1,11 @@
 package com.telefonica.mistica.testutils
 
+import com.telefonica.mistica.compose.button.ButtonStyle
+import com.telefonica.mistica.compose.theme.brand.BlauBrand
+import com.telefonica.mistica.compose.theme.brand.MovistarBrand
+import com.telefonica.mistica.compose.theme.brand.O2Brand
+import com.telefonica.mistica.compose.theme.brand.TelefonicaBrand
+import com.telefonica.mistica.compose.theme.brand.VivoBrand
 import org.junit.Test
 
 object TestUtils {
@@ -20,5 +26,21 @@ object TestUtils {
             }
         }
         throw IllegalStateException("Could not find running test method name")
+    }
+    fun getAllBrands() = listOf(
+        MovistarBrand,
+        VivoBrand,
+        O2Brand,
+        BlauBrand,
+        TelefonicaBrand,
+    )
+
+    fun ButtonStyle.isInverse() = when(this) {
+        ButtonStyle.PRIMARY_INVERSE,
+        ButtonStyle.PRIMARY_SMALL_INVERSE,
+        ButtonStyle.SECONDARY_INVERSE,
+        ButtonStyle.SECONDARY_SMALL_INVERSE,
+        ButtonStyle.LINK_INVERSE -> true
+        else -> false
     }
 }
