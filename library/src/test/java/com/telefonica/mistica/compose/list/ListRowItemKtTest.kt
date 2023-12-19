f56282c512d2c1ee887ecee21cbcfa340415fbf9
Josep Rubio <jose.rubiomorente@telefonica.com>
2023-12-19 12:33:15 +0100

    ANDROID-14098 add roborazzi tests for ListRowItem (compose)

---
+++ library/src/test/java/com/telefonica/mistica/compose/list/ListRowItemKtTest.kt
@@ -0,0 +1,61 @@
+package com.telefonica.mistica.compose.list
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import com.telefonica.mistica.compose.shape.Chevron
+import com.telefonica.mistica.compose.tag.Tag
+import com.telefonica.mistica.compose.theme.MisticaTheme
+import com.telefonica.mistica.compose.theme.brand.MovistarBrand
+import com.telefonica.mistica.list.model.ImageDimensions
+import com.telefonica.mistica.testutils.ScreenshotsTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import com.telefonica.mistica.R
+
+@RunWith(RobolectricTestRunner::class)
+internal class ListRowItemKtTest: ScreenshotsTest() {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun `check ListRowItem with 64x64 asset`() {
+        `when ListRowItem with asset`(ImageDimensions(width = 64, height = 64))
+
+        `then screenshot is OK`()
+    }
+
+    @Test
+    fun `check ListRowItem with 32x32 asset`() {
+        `when ListRowItem with asset`(ImageDimensions(width = 32, height = 32))
+
+        `then screenshot is OK`()
+    }
+
+    @OptIn(ExperimentalMaterialApi::class)
+    private fun `when ListRowItem with asset`(dimensions: ImageDimensions) {
+        composeTestRule.setContent {
+            MisticaTheme(brand = MovistarBrand) {
+                ListRowItem(
+                    listRowIcon = ListRowIcon.ImageAsset(
+                        painter = painterResource(id = R.drawable.placeholder),
+                        dimensions = ImageDimensions(width = dimensions.width, height = dimensions.height),
+                    ),
+                    headline = Tag("Promo"),
+                    isBadgeVisible = true,
+                    title = "Title",
+                    subtitle = "Subtitle",
+                    description = "Description",
+                    trailing = { Chevron() },
+                )
+            }
+        }
+    }
+
+    private fun `then screenshot is OK`() {
+        compareScreenshot(composeTestRule.onRoot())
+    }
+}
