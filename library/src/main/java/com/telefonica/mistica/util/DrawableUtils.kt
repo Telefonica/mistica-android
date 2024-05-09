88f6e17fd08fbee190850700b52eaede9a839aab
David Pastor Herranz <David.Pastor.Herranz@telefonica.com>
2024-05-09 15:51:37 +0200

    ANDROID-14558 Just expose PaintDrawable as return type for drawables to allow more customization flexibility on consumer apps.

--- library/src/main/java/com/telefonica/mistica/util/DrawableUtils.kt
+++ library/src/main/java/com/telefonica/mistica/util/DrawableUtils.kt
@@ -1,20 +1,16 @@
 package com.telefonica.mistica.util
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.graphics.drawable.PaintDrawable
 import android.util.TypedValue
 import androidx.annotation.AttrRes
 import androidx.appcompat.content.res.AppCompatResources
 
 fun Context.getMisticaThemeDrawableBuilder(@AttrRes themeDrawable: Int): DrawableBuilder {
-    return DrawableBuilder(this, getMisticaThemePaintDrawable(themeDrawable))
+    return DrawableBuilder(this, getMisticaThemeDrawable(themeDrawable))
 }
 
-fun Context.getMisticaThemeDrawable(@AttrRes themeDrawable: Int): Drawable =
-    getMisticaThemePaintDrawable(themeDrawable)
-
-private fun Context.getMisticaThemePaintDrawable(@AttrRes themeDrawable: Int): PaintDrawable {
+fun Context.getMisticaThemeDrawable(@AttrRes themeDrawable: Int): PaintDrawable {
     val typedValue = TypedValue()
     theme.resolveAttribute(themeDrawable, typedValue, true)
     if (typedValue.data == TypedValue.DATA_NULL_UNDEFINED) {
