0134a51f8700165653e95edccda49d54b65fb59d
Pablo Garcia <pablo.garcianunez@telefonica.com>
2024-07-31 18:57:47 +0200

    ANDROID-15025 add number input (#373)
    * ANDROID-15025 add number input
    
    * Modify numeric type to decimal
    
    * Remove regex

---
+++ library/src/main/java/com/telefonica/mistica/compose/input/NumberInput.kt
@@ -0,0 +1,46 @@
+package com.telefonica.mistica.compose.input
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.core.text.isDigitsOnly
+
+@Composable
+fun NumberInput(
+    modifier: Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    helperText: String? = null,
+    isError: Boolean = false,
+    errorText: String? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    isInverse: Boolean = false,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    onClick: (() -> Unit)? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+) {
+
+    TextInputImpl(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        label = label,
+        helperText = helperText,
+        isError = isError,
+        errorText = errorText,
+        trailingIcon = trailingIcon,
+        isInverse = isInverse,
+        enabled = enabled,
+        readOnly = readOnly,
+        onClick = onClick,
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions.toFoundationKeyboardOptions(
+            keyboardType = KeyboardType.Decimal
+        )
+    )
+
+}
