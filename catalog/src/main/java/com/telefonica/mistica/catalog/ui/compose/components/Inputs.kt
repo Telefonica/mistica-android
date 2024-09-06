15e5430a911a49ec5929ddf41522b1694b5a5caf
yamal <yamal@tuenti.com>
2024-09-06 12:31:08 +0200

    ANDROID-15138 Search Input in compose

--- catalog/src/main/java/com/telefonica/mistica/catalog/ui/compose/components/Inputs.kt
+++ catalog/src/main/java/com/telefonica/mistica/catalog/ui/compose/components/Inputs.kt
@@ -31,6 +31,7 @@ import com.telefonica.mistica.compose.input.LimitCharacters
 import com.telefonica.mistica.compose.input.NumberInput
 import com.telefonica.mistica.compose.input.PasswordInput
 import com.telefonica.mistica.compose.input.PhoneInput
+import com.telefonica.mistica.compose.input.SearchInput
 import com.telefonica.mistica.compose.input.TextAreaInput
 import com.telefonica.mistica.compose.input.TextInput
 import com.telefonica.mistica.compose.input.TextLink
@@ -81,6 +82,8 @@ fun Inputs() {
         DropDownSample()
         Title("Disable Dropdown")
         DropDownSample(enabled = false)
+        Title("Search Input")
+        SearchInputSample()
         Title("Inverse inputs")
         Column(
             modifier = Modifier
@@ -443,5 +446,24 @@ private fun DropDownSample(enabled: Boolean = true) {
             selectedItemIndex = it
         },
         enabled = enabled
+    )
+}
+
+@Composable
+private fun SearchInputSample() {
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    SearchInput(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, start = 16.dp, end = 16.dp)
+        ,
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = "Search Something"
     )
 }
---
+++ library/src/main/java/com/telefonica/mistica/compose/input/SearchInput.kt
@@ -0,0 +1,54 @@
+package com.telefonica.mistica.compose.input
+
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import com.telefonica.mistica.R
+import com.telefonica.mistica.compose.theme.MisticaTheme
+
+@Composable
+fun SearchInput(
+    modifier: Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String?,
+    helperText: String? = null,
+    isError: Boolean = false,
+    errorText: String? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    isInverse: Boolean = false,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    onClick: (() -> Unit)? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+) {
+    TextInputImpl(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        label = label,
+        helperText = helperText,
+        isError = isError,
+        errorText = errorText,
+        trailingIcon = trailingIcon,
+        isInverse = isInverse,
+        enabled = enabled,
+        readOnly = readOnly,
+        onClick = onClick,
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions.toFoundationKeyboardOptions(
+            keyboardType = KeyboardType.Text
+        ),
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_search_regular),
+                tint = MisticaTheme.colors.neutralHigh,
+                contentDescription = null
+            )
+        }
+    )
+}
