16231f8d418da9481ca5204929a5d2a93be69214
Jose Rubio <jose.rubiomorente@telefonica.com>
2024-09-12 15:28:38 +0200

    ANDROID-15168 compose badge improvements (#383)
    * ANDROID-15168 expose badge text size
    
    * ANDROID-15168 trim badgecontent if numeric and > 9
    
    * ANDROID-15168 add more options to the catalog
    
    * ANDROID-15168 remove ExperimentalMaterialApi annotation
    
    * ANDROID-15168 add screenshots tests
    
    * Updated screenshots baseline
    
    * ANDROID-15168 add endline
    
    * ANDROID-15168 rename tests (and screenshots)
    
    * ANDROID-15168 rename tests (and screenshots)
    
    ---------
    
    Co-authored-by: jeprubio <jeprubio@users.noreply.github.com>

--- catalog/src/main/java/com/telefonica/mistica/catalog/ui/compose/components/Badges.kt
+++ catalog/src/main/java/com/telefonica/mistica/catalog/ui/compose/components/Badges.kt
@@ -7,12 +7,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BadgedBox
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.telefonica.mistica.catalog.R
 import com.telefonica.mistica.compose.badge.Badge
+import com.telefonica.mistica.compose.button.Button
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -24,9 +33,44 @@ fun Badges() {
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        var badgeContent: String? by rememberSaveable { mutableStateOf(null) }
+        var show: Boolean by rememberSaveable { mutableStateOf(false) }
 
         Spacer(modifier = Modifier.height(16.dp))
-        Badge()
-        Badge(content = "1")
+        BadgedBox(
+            badge = {
+                if (show) {
+                    Badge(content = badgeContent)
+                }
+            }
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.icn_creditcard),
+                contentDescription = null,
+            )
+        }
+        Button(
+            text = "Toggle",
+            onClickListener = {
+                badgeContent = ""
+                show = !show
+            }
+        )
+
+        Button(
+            text = "Show with one digit number",
+            onClickListener = {
+                show = true
+                badgeContent = (0..9).random().toString()
+            }
+        )
+
+        Button(
+            text = "Show with two digit number",
+            onClickListener = {
+                show = true
+                badgeContent = (10..99).random().toString()
+            }
+        )
     }
 }
