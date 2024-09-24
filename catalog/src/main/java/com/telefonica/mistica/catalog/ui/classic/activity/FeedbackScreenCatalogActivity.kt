910fdad97be26a59943fffff1891c9cb7eda3e91
jmanrique <jmanrique@hiberus.com>
2024-09-24 10:19:45 +0200

    ANDROID-11179 unused import removed

--- catalog/src/main/java/com/telefonica/mistica/catalog/ui/classic/activity/FeedbackScreenCatalogActivity.kt
+++ catalog/src/main/java/com/telefonica/mistica/catalog/ui/classic/activity/FeedbackScreenCatalogActivity.kt
@@ -2,7 +2,6 @@ package com.telefonica.mistica.catalog.ui.classic.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import com.telefonica.mistica.catalog.R
