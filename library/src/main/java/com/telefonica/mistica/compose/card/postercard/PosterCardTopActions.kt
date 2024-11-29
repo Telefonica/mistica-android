bdea242555783b56b0977f7535c0c613104849ed
David Hontiveros Gordo <dhontiverosg@axpe.com>
2024-11-29 09:50:56 +0100

    dhonti/ANDROID-15405-postercard: Remove unused imports

--- library/src/main/java/com/telefonica/mistica/compose/card/postercard/PosterCardTopActions.kt
+++ library/src/main/java/com/telefonica/mistica/compose/card/postercard/PosterCardTopActions.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.focused
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.unit.dp
