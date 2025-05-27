ab39002004c5786e8abed6301b79c001d892603f
jdelga <jdelga@users.noreply.github.com>
2025-05-27 15:14:26 +0000

    Update design tokens

---
+++ library/src/main/java/com/telefonica/mistica/compose/theme/brand/O2NewBrandThemeVariantAutogenerated.kt
@@ -0,0 +1,11 @@
+// Code generated automatically, DO NOT EDIT manually. Use Import design tokens GitHub Action.
+package com.telefonica.mistica.compose.theme.brand
+
+import androidx.compose.ui.unit.dp
+import com.telefonica.mistica.compose.theme.values.MisticaThemeVariant
+
+public object O2NewBrandThemeVariant {
+  public val themeVariant: MisticaThemeVariant = MisticaThemeVariant(
+      successFeedbackThemeVariant = "inverse",
+      brandLoadingScreenThemeVariant = "inverse",)
+}
