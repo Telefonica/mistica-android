b47359c22d3ebda9afd6ee417f44832673beb226
jdelga <jdelga@users.noreply.github.com>
2025-05-28 11:10:30 +0000

    Update design tokens

--- library/src/main/java/com/telefonica/mistica/compose/theme/brand/MovistarBrandThemeVariantAutogenerated.kt
+++ library/src/main/java/com/telefonica/mistica/compose/theme/brand/MovistarBrandThemeVariantAutogenerated.kt
@@ -1,11 +1,11 @@
 // Code generated automatically, DO NOT EDIT manually. Use Import design tokens GitHub Action.
 package com.telefonica.mistica.compose.theme.brand
 
-import androidx.compose.ui.unit.dp
 import com.telefonica.mistica.compose.theme.values.MisticaThemeVariant
+import com.telefonica.mistica.compose.theme.values.ThemeVariant
 
 public object MovistarBrandThemeVariant {
   public val themeVariant: MisticaThemeVariant = MisticaThemeVariant(
-      successFeedbackThemeVariant = "default",
-      brandLoadingScreenThemeVariant = "default",)
+      successFeedbackThemeVariant = ThemeVariant.DEFAULT,
+      brandLoadingScreenThemeVariant = ThemeVariant.DEFAULT,)
 }
