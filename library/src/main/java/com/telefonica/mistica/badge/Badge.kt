0f363b0191d4de667188a96577b583e9c6e088e0
Pablo Martín <pablo.martinblanco@telefonica.com>
2025-07-22 11:28:24 +0200

    Add trailing comma

--- library/src/main/java/com/telefonica/mistica/badge/Badge.kt
+++ library/src/main/java/com/telefonica/mistica/badge/Badge.kt
@@ -64,7 +64,7 @@ object Badge {
 
     private fun createBadge(
         anchor: View,
-        count: Int
+        count: Int,
     ) = BadgeDrawable.create(anchor.context).apply {
         maxCharacterCount = 2
         backgroundColor = anchor.context.getThemeColor(R.attr.colorBadge)
