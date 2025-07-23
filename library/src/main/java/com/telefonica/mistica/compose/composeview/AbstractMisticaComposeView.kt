2a40eb5a8fd6d28ab56d762a0596308f106f633c
Borja Bravo <borja.bravoalvarez@telefonica.com>
2025-07-23 11:56:47 +0200

    feat: match fields between attributes from xml and composeView

--- library/src/main/java/com/telefonica/mistica/compose/composeview/AbstractMisticaComposeView.kt
+++ library/src/main/java/com/telefonica/mistica/compose/composeview/AbstractMisticaComposeView.kt
@@ -10,6 +10,7 @@ import com.telefonica.mistica.compose.composeview.AbstractMisticaComposeView.Com
 import com.telefonica.mistica.compose.composeview.AbstractMisticaComposeView.Companion.BRAND_VALUE_MOVISTAR
 import com.telefonica.mistica.compose.composeview.AbstractMisticaComposeView.Companion.BRAND_VALUE_O2_NEW
 import com.telefonica.mistica.compose.composeview.AbstractMisticaComposeView.Companion.BRAND_VALUE_TELEFONICA
+import com.telefonica.mistica.compose.composeview.AbstractMisticaComposeView.Companion.BRAND_VALUE_TU
 import com.telefonica.mistica.compose.composeview.AbstractMisticaComposeView.Companion.BRAND_VALUE_VIVO_NEW
 import com.telefonica.mistica.compose.theme.MisticaTheme
 import com.telefonica.mistica.compose.theme.brand.BlauBrand
@@ -17,6 +18,7 @@ import com.telefonica.mistica.compose.theme.brand.Brand
 import com.telefonica.mistica.compose.theme.brand.MovistarBrand
 import com.telefonica.mistica.compose.theme.brand.O2NewBrand
 import com.telefonica.mistica.compose.theme.brand.TelefonicaBrand
+import com.telefonica.mistica.compose.theme.brand.TuBrand
 import com.telefonica.mistica.compose.theme.brand.VivoNewBrand
 
 abstract class AbstractMisticaComposeView @JvmOverloads constructor(
@@ -37,6 +39,7 @@ abstract class AbstractMisticaComposeView @JvmOverloads constructor(
         BRAND_VALUE_BLAU,
         BRAND_VALUE_O2_NEW,
         BRAND_VALUE_VIVO_NEW,
+        BRAND_VALUE_TU,
     )
     annotation class BrandConfig
 
@@ -65,6 +68,7 @@ abstract class AbstractMisticaComposeView @JvmOverloads constructor(
         const val BRAND_VALUE_BLAU = 2
         const val BRAND_VALUE_O2_NEW = 3
         const val BRAND_VALUE_VIVO_NEW = 4
+        const val BRAND_VALUE_TU = 5
     }
 }
 
@@ -74,5 +78,6 @@ fun Int.mapToComposeBrand(): Brand = when (this) {
     BRAND_VALUE_BLAU -> BlauBrand
     BRAND_VALUE_O2_NEW -> O2NewBrand
     BRAND_VALUE_VIVO_NEW -> VivoNewBrand
+    BRAND_VALUE_TU -> TuBrand
     else -> TelefonicaBrand
 }
