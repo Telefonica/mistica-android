011f1b21d59e8a454dfff52f207520b04f7b203f
Pablo Martín <pablo.martinblanco@telefonica.com>
2025-07-29 11:28:23 +0200

    Unused import

--- catalog/src/main/java/com/telefonica/mistica/catalog/ui/compose/components/Badges.kt
+++ catalog/src/main/java/com/telefonica/mistica/catalog/ui/compose/components/Badges.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.telefonica.mistica.catalog.R
