a3a1eee51aed244f524ddf6d5e341318b50393c5
Pablo Martín <pablo.martinblanco@telefonica.com>
2025-08-13 10:43:54 +0200

    Configure Robolectric test for target SDK 36

--- library/src/test/java/com/telefonica/mistica/compose/button/ButtonBehaviourTest.kt
+++ library/src/test/java/com/telefonica/mistica/compose/button/ButtonBehaviourTest.kt
@@ -1,5 +1,6 @@
 package com.telefonica.mistica.compose.button
 
+import android.os.Build.VERSION_CODES.VANILLA_ICE_CREAM
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -13,8 +14,10 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [VANILLA_ICE_CREAM])
 internal class ButtonBehaviourTest {
     @get:Rule
     val composeTestRule = createComposeRule()
