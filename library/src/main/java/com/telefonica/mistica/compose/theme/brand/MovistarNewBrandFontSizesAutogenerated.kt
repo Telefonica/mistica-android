289f64270d55714f86b839b7e14d7caba71aea1e
jdelga <jdelga@users.noreply.github.com>
2025-09-30 14:55:49 +0000

    Update design tokens

---
+++ library/src/main/java/com/telefonica/mistica/compose/theme/brand/MovistarNewBrandFontSizesAutogenerated.kt
@@ -0,0 +1,31 @@
+// Code generated automatically, DO NOT EDIT manually. Use Import design tokens GitHub Action.
+package com.telefonica.mistica.compose.theme.brand
+
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+public object MovistarNewBrandFontSizes {
+  public val tabsLabelFontSize: TextUnit = 16.sp
+
+  public val title3FontSize: TextUnit = 20.sp
+
+  public val text1FontSize: TextUnit = 12.sp
+
+  public val text2FontSize: TextUnit = 14.sp
+
+  public val text3FontSize: TextUnit = 16.sp
+
+  public val text4FontSize: TextUnit = 18.sp
+
+  public val text5FontSize: TextUnit = 20.sp
+
+  public val text6FontSize: TextUnit = 24.sp
+
+  public val text7FontSize: TextUnit = 28.sp
+
+  public val text8FontSize: TextUnit = 32.sp
+
+  public val text9FontSize: TextUnit = 40.sp
+
+  public val text10FontSize: TextUnit = 48.sp
+}
---
+++ library/src/main/java/com/telefonica/mistica/compose/theme/brand/MovistarNewBrandRadiusAutogenerated.kt
@@ -0,0 +1,22 @@
+// Code generated automatically, DO NOT EDIT manually. Use Import design tokens GitHub Action.
+package com.telefonica.mistica.compose.theme.brand
+
+import androidx.compose.ui.unit.dp
+import com.telefonica.mistica.compose.theme.values.MisticaRadius
+
+public object MovistarNewBrandRadius {
+  public val radius: MisticaRadius = MisticaRadius(
+      avatarBorderRadius = 50,
+      barBorderRadius = 999.dp,
+      buttonBorderRadius = 4.dp,
+      checkboxBorderRadius = 4.dp,
+      containerBorderRadius = 16.dp,
+      indicatorBorderRadius = 24.dp,
+      chipBorderRadius = 24.dp,
+      tagBorderRadius = 0.dp,
+      inputBorderRadius = 12.dp,
+      legacyDisplayBorderRadius = 16.dp,
+      popupBorderRadius = 8.dp,
+      sheetBorderRadius = 16.dp,
+      mediaSmallBorderRadius = 8.dp,)
+}
